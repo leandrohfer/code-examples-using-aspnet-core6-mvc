45f63cd0e33ed74004fa86e0490adfa658d1d001
Leandro Henrique <leandro.pinheiro@santacasa.pa.gov.br>
2022-09-23 02:04:26 -0300

    Exemplo finalizado

---
+++ WebApplicationUsingPostgreSQL/WebApplicationUsingPostgreSQL/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplicationUsingPostgreSQL.Models
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options) : base(options)
+        {
+        }
+        
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApplicationUsingPostgreSQL/WebApplicationUsingPostgreSQL/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationUsingPostgreSQL.Models
+{
+    [Table("Product")]
+    public class Product
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Column("Name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+    }
+}
